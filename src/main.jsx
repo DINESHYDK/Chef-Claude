b6aabe4c20fef680d63df711f27b24897dfebafa
YDK <dineshkrishnayeturi@gmail.com>
2025-06-23 19:04:04 +0530

    completed the header section

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,36 @@
 export default function Main() {
+  const ingredients = ["Chicken", "Tomatoes", "Potatoes", "Onions"];
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event) {
+    // Fixed parameter name
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    ingredients.push(newIngredient);
+  }
+
   return (
     <>
       <main>
-        <form className="add-ingredient-form">
-          <input 
-          type="text" 
-          placeholder="e.g. Onions"
-          aria-label="New Ingredient"
+        <form onSubmit={handleSubmit} className="add-ingredient-form">
+          <input
+            type="text"
+            placeholder="e.g. oregano"
+            aria-label="New Ingredient"
+            name="ingredient"
           />
-          <button>Add Ingredient</button>
+          <button>+ Add ingredient</button>
         </form>
+
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list">{ingredientsListItems}</ul>
+        </section>
       </main>
     </>
   );
